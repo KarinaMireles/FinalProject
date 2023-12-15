df3c3a8210a3c2d5a560f0b654e00921312e35fb
Joshua Propst <jpropst35@gmail.com>
2023-12-14 19:02:19 -0500

    firebase and mongoDB setup

---
+++ Backend/functions/src/models/FinalProject.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const FinalProjectSchema = new mongoose.Schema(
+	{
+		message: {
+			type: String,
+			required: true
+		}
+	},
+	{ timestamps: true }
+)
+
+FinalProjectSchema.set("toJSON", {
+	transform: function (doc, ret) {
+		ret.id = ret._id.toHexString()
+		delete ret._id
+		delete ret.__v
+	}
+})
+
+const FinalProject = mongoose.model("Final_Project", FinalProjectSchema)
+export default FinalProject
